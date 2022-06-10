f0c415b1bc0d368bc548a5c1f557d3370e717829
cater20 <onyangoketta@gmail.com>
2022-06-10 12:26:37 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function divide(a,b) {
-    const a=2000
-    const b=100
-    return a/b
-   const square=(x,2)<=x*2
-   let x=2
-   square (x)
-}let a=3
-let b=4
-const add=(a,b)<=a+b
-add(3+4)
+const divide=(num1=2000,num2=100)=>{
+    return num1/num2
+}
+let x=2;
+const square = ( x) =>  (x * x) 
+const add = ( a,b ) => (a + b )
