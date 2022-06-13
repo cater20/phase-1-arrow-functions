af42dcb93e9ad061ae3456b3c46a8385c61f7fa7
cater20 <onyangoketta@gmail.com>
2022-06-13 13:16:00 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const divide=(num1=2000,num2=100)=>{
-    return num1/num2
+    return num1/num2;
 }
 let x=2;
-const square = ( x) =>  (x * x) 
-const add = ( a,b ) => (a + b )
+const square = ( x) =>  (x * x) ;
+const add = ( a,b ) => (a + b );
